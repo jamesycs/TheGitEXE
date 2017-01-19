873d7fa52ed14aae72b4230c1aa0c155984adf74
jamesycs <kaynejames@Kaynes-MacBook-Pro.local>
2017-01-19 20:43:59 +0800

    change1

--- TheGitEXE/ViewController.swift
+++ TheGitEXE/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
-		print("HI_conflict")
+		print("HI_conflict123")
 		rint("HI2")
 	}
 
