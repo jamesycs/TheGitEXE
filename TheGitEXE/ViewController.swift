3833eb40eb674caaf07b2e013e43f2805fc4ce48
jamesycs <kaynejames@Kaynes-MacBook-Pro.local>
2017-01-19 21:06:52 +0800

    Codera sakkalaka

--- TheGitEXE/ViewController.swift
+++ TheGitEXE/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		print("BIM")
+		saklaka
 		rint("HI2")
 		Print("V")
 	}
