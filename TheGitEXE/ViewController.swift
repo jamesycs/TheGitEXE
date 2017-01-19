24d892a647f9bbe889a1379215b4a4de70b0679a
jamesycs <kaynejames@Kaynes-MacBook-Pro.local>
2017-01-19 21:07:04 +0800

    Coder added many things

--- TheGitEXE/ViewController.swift
+++ TheGitEXE/ViewController.swift
@@ -13,14 +13,16 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
-		print("BIM")
+		print("BIM""sadsadasd")
 		rint("HI2")
+		Print("V")
 		Print("V")
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
+		rint("HI2")
 	}
 
 
